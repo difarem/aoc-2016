34e82a0c2022c83f9522493b98da0f617d6f2b05
Difarem <difarem@gmail.com>
2016-12-04 11:55:35 +0100

    day 3 fmt

--- day03_2.go
+++ day03_2.go
@@ -6,7 +6,7 @@ import (
 
 func validTriangle(a, b, c int) bool {
 	fmt.Println("checking", a, b, c)
-	
+
 	if a+b <= c {
 		return false
 	} else if a+c <= b {
@@ -20,12 +20,10 @@ func validTriangle(a, b, c int) bool {
 func main() {
 	var matrix [3][3]int
 	valid := 0
-	for _, err := fmt.Scanln(&matrix[0][0], &matrix[0][1], &matrix[0][2]);
-			err == nil;
-			_, err = fmt.Scanln(&matrix[0][0], &matrix[0][1], &matrix[0][2]) {
+	for _, err := fmt.Scanln(&matrix[0][0], &matrix[0][1], &matrix[0][2]); err == nil; _, err = fmt.Scanln(&matrix[0][0], &matrix[0][1], &matrix[0][2]) {
 		fmt.Scanln(&matrix[1][0], &matrix[1][1], &matrix[1][2])
 		fmt.Scanln(&matrix[2][0], &matrix[2][1], &matrix[2][2])
-		
+
 		for j := 0; j < 3; j++ {
 			if validTriangle(matrix[0][j], matrix[1][j], matrix[2][j]) {
 				valid++
